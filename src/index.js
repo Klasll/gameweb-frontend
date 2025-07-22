56d5feff1dff1940154d207f86f8b33515a94d8b
Klasll <115462054+Klasll@users.noreply.github.com>
2025-07-23 00:46:42 +0200

    y

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 console.log("index.js geladen");
 
-import App from "./AppTemp";
+import App from "./AppTemp.jsx";
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
