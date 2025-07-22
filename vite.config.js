65ac48fb9058151529560d0e960ca8b40f81715e
Klasll <115462054+Klasll@users.noreply.github.com>
2025-07-23 00:25:18 +0200

    y

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: './', // Root bleibt Projekt-Root
+  publicDir: 'public', // Hier liegt index.html
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
