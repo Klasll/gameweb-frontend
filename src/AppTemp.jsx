4b12aedd0cf36986a964a0158631dea02e571d41
Klasll <115462054+Klasll@users.noreply.github.com>
2025-07-23 00:13:22 +0200

    y

--- src/AppTemp.jsx
+++ src/AppTemp.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import StartScreen from "./pages/StartScreen";
 import GameScreen from "./pages/GameScreen";
 
-export default function App() {
+export default function AppTemp() {
   const [started, setStarted] = useState(false);
+
+  console.log("started:", started); // Debug-Ausgabe
 
   return (
     <>
