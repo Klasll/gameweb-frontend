5e290a496480fe2b45435367e5c8ee2fe367f4bd
Klasll <115462054+Klasll@users.noreply.github.com>
2025-07-23 00:44:03 +0200

    y

--- src/AppTemp.jsx
+++ src/AppTemp.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-console.log("AppTemp Komponente geladen");
-
 import StartScreen from "./pages/StartScreen";
 import GameScreen from "./pages/GameScreen";
 
 export default function App() {
   const [started, setStarted] = useState(false);
+
   console.log("AppTemp render");
 
   return (
     <>
+      <div style={{ color: "white", fontSize: "24px" }}>TEST SICHTBAR</div>
       {!started ? (
         <StartScreen onStart={() => setStarted(true)} />
       ) : (
